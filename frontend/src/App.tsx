a82ac13ece2ce92ec65602ec5649c2b3e9c95848
RobbeHeirman <robbeheirman@msn.com>
2024-08-13 18:54:46 +0200

    can connect to server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,44 @@
 import './App.css'
-import { JoinLobbyMessage } from './proto/user_actions.ts'
+import {useState} from "react";
+import {JoinLobbyMessage} from "./proto/user_actions.ts";
+
 
 // const socket = new WebSocket("ws://localhost:8080");
-
-const msg: JoinLobbyMessage =  {
-    name: "robbe"
-}
-
-const b = JoinLobbyMessage.toBinary(msg)
-console.log(b);
-
 
 
 function App() {
     // socket.addEventListener("open", () => socket.send("Hello from client!")
-// )
+    const [socket, setSocket] = useState<WebSocket | null>(null)
+
+    function onSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const formProps = Object.fromEntries(new FormData(e.currentTarget));
+        const connectMessage: JoinLobbyMessage = {
+            name: formProps.name.toString()
+        }
+        const protoMsg = JoinLobbyMessage.toBinary(connectMessage);
+        const nwSocket = new WebSocket("ws://localhost:8080");
+
+        nwSocket.addEventListener("open", () => {
+            setSocket(nwSocket);
+            nwSocket.send(protoMsg)
+        })
+    }
+
+    if (socket === null) {
+        return (
+            <>
+                <form onSubmit={onSubmit}>
+                    <label>name: </label>
+                    <input type={"text"} id={"name"} name={"name"}/>
+                    <button type={"submit"}>Submit</button>
+                </form>
+            </>
+        )
+    }
     return (
         <>
-            <label>name: </label>
-            <input type={"text"}/>
-            <button>Submit</button>
+            <p>Connected!</p>
         </>
     )
 }

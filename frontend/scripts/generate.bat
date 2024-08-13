b7428d4dda77df9f723474f78441f7d18d637acd
RobbeHeirman <robbeheirman@msn.com>
2024-08-13 17:37:23 +0200

    fixing proto generation

--- frontend/scripts/generate.bat
+++ frontend/scripts/generate.bat
@@ -1,7 +1,7 @@
-set TARGET_DIR=.\src\protobuf
+set TARGET_DIR=.\src\proto
 mkdir "%TARGET_DIR%"
 if not exist "%TARGET_DIR%" (
     mkdir "%TARGET_DIR%"
 )
 
-for %%f in (..\protobuff\*.proto) do protoc --plugin=protoc-gen-ts=.\node_modules\.bin\protoc-gen-ts.cmd --ts_opt=esModuleInterop=true --ts_out=".\src\protobuf" -I ..\protobuff %%f
+for %%f in (..\protobuf\*.proto) do npx protoc --ts_out %TARGET_DIR% --proto_path ..\protobuf %%f
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
+import { JoinLobbyMessage } from './proto/user_actions.ts'
 
+// const socket = new WebSocket("ws://localhost:8080");
 
-const socket = new WebSocket("ws://localhost:8080");
+const msg: JoinLobbyMessage =  {
+    name: "robbe"
+}
+
+const b = JoinLobbyMessage.toBinary(msg)
+console.log(b);
+
 
 
 function App() {
-    socket.addEventListener("open", () => socket.send("Hello from client!")
-)
+    // socket.addEventListener("open", () => socket.send("Hello from client!")
+// )
     return (
         <>
             <label>name: </label>
